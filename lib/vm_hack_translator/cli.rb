542371edad49bd5c199b588e31efb5618a2fe7c3
nekootoko3 <miyako.kerria@gmail.com>
2020-03-23 22:50:24 +0900

    Implemented i/o

--- lib/vm_hack_translator/cli.rb
+++ lib/vm_hack_translator/cli.rb
@@ -1,5 +1,23 @@
+require "vm_hack_translator/parser"
+require "vm_hack_translator/command_type"
+
 module VmHackTranslator::Cli
   def self.start(args)
-    p args
+    input_files = case File.ftype(args[0])
+      when "file"
+        raise "Invalid file type passed" unless args[0].match?(/.+\.vm/)
+
+        [args[0]]
+      when "directory"
+        Dir.chdir(args[0])
+        Dir.glob("*.vm")
+      else
+        raise "Invalid input specified"
+      end
+    output = args[1].nil? ? $stdout : File.open(args[1], "w+")
+
+    input_files.each do |input_file|
+      parser = VmHackTranslator::Parser.new(input_file)
+    end
   end
 end
