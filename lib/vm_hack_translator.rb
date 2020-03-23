76f069adc331ac81e792c78196e17c84e94182e6
nekootoko3 <miyako.kerria@gmail.com>
2020-03-23 12:49:22 +0900

    Exec bundle gem

---
+++ lib/vm_hack_translator.rb
@@ -0,0 +1,6 @@
+require "vm_hack_translator/version"
+
+module VmHackTranslator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vm_hack_translator/version.rb
@@ -0,0 +1,3 @@
+module VmHackTranslator
+  VERSION = "0.1.0"
+end
