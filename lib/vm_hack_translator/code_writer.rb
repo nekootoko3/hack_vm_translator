13e40db7d3faa87cd5d36f046d24891b2d14f5a4
nekootoko3 <miyako.kerria@gmail.com>
2020-03-25 12:11:27 +0900

    Implemented segment pointer

--- lib/vm_hack_translator/code_writer.rb
+++ lib/vm_hack_translator/code_writer.rb
@@ -89,10 +89,10 @@ class VmHackTranslator::CodeWriter
   def output_initialize!
     @output.puts(
       "@#{INITIAL_STACK_POINTER}", "D=A", "@SP", "M=D",
-      "@#{LOCAL_BASE}", "D=A", "@LCL", "M=D",
-      "@#{ARGUMENT_BASE}", "D=A", "@ARG", "M=D",
-      "@#{THIS_BASE}", "D=A", "@THIS", "M=D",
-      "@#{THAT_BASE}", "D=A", "@THAT", "M=D",
+#      "@#{LOCAL_BASE}", "D=A", "@LCL", "M=D",
+#      "@#{ARGUMENT_BASE}", "D=A", "@ARG", "M=D",
+#      "@#{THIS_BASE}", "D=A", "@THIS", "M=D",
+#      "@#{THAT_BASE}", "D=A", "@THAT", "M=D",
     )
   end
 
@@ -100,6 +100,10 @@ class VmHackTranslator::CodeWriter
     case segment.to_sym
     when :constant
       @output.puts("@#{value}", "D=A")
+    when :pointer
+      @output.puts("@#{symbol_from(segment)}")
+      @output.puts(["A=A+1"] * value.to_i) if value.to_i > 0
+      @output.puts("D=M")
     else
       @output.puts("@#{symbol_from(segment)}", "A=M")
       @output.puts(["A=A+1"] * value.to_i) if value.to_i > 0
@@ -114,8 +118,8 @@ class VmHackTranslator::CodeWriter
   def write_pop!(segment, value)
     @output.puts(
       "@SP", "A=M-1", "D=M // D <- pop value",
-      "@#{symbol_from(segment)}", "A=M"
-    )
+      "@#{symbol_from(segment)}")
+    @output.puts("A=M") unless [:pointer, :temp].include?(segment.to_sym)
     @output.puts(["A=A+1"] * value.to_i) if value.to_i > 0
     @output.puts("M=D", "@SP", "M=M-1")
   end
--- lib/vm_hack_translator/memory_accessor.rb
+++ lib/vm_hack_translator/memory_accessor.rb
@@ -4,28 +4,13 @@ class VmHackTranslator::MemoryAccessor
     argument: "ARG",
     this: "THIS",
     that: "THAT",
-    temp: "5"
+    temp: "5",
+    pointer: "3",
   }
 
-  VALID_SEGMENTS = [
-    :local,
-    :argument,
-    :this,
-    :that,
-    :temp,
-    :pointer,
-  ]
-
-  INITIAL_TEMP_ADDRESS = 5
-
   def self.symbol_from(segment)
-    raise "Invalid segment #{segment}" unless VALID_SEGMENTS.include?(segment.to_sym)
+    raise "Invalid segment #{segment}" unless SEGMENT_SYMBOL_MAPPING.keys.include?(segment.to_sym)
 
-    case segment.to_sym
-    when :temp
-      INITIAL_TEMP_ADDRESS
-    else
-      SEGMENT_SYMBOL_MAPPING[segment.to_sym]
-    end
+    SEGMENT_SYMBOL_MAPPING[segment.to_sym]
   end
 end
